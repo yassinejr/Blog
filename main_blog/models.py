4a91e184f0fd7abc5f428f32b22eb573897c36fa
yassinejr <yassine.jrad@esprit.tn>
2020-12-18 00:21:36 +0100

    add generic views: add, edit, delete and update posts

--- main_blog/models.py
+++ main_blog/models.py
@@ -1,5 +1,7 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 
 # Create your models here.
@@ -8,6 +10,12 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        # return reverse('article-detail', args=[str(self.id)])
+        return reverse('home')
