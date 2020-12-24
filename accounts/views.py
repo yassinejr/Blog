99b7a24c0ec381adc4dd74f72c8fc07307405bb7
yassinejr <yassine.jrad@esprit.tn>
2020-12-25 00:44:19 +0100

    add change password template

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
 from .forms import *
 
 
@@ -19,3 +20,12 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
